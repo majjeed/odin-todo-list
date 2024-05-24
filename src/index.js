f26468d88c3c9db862c8b0f151c87fadbe3449a7
majjeed <abdulmajeed.hersi@gmail.com>
2024-05-24 15:19:19 +0100

    Quick Fix to load images in template html
    html-loader is a possible alternative

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import Icon from "./images/sailboat-boat-svgrepo-com.svg";
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    // const logo = document.querySelector('.logo-img');
+    // logo.src = Icon;
+
+    return element;
+}
+
+document.body.appendChild(component());
